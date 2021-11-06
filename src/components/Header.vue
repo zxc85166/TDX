7ace6955ca88e401f9785944886a77bbb9efcae1
iiiteam11801 <iiiteam11801@gmail.com>
2021-11-06 20:48:58 +0800

    router-link to="/"

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,14 +3,15 @@
 </script>
 <template>
     <div class="grid grid-rows-2 md:grid-rows-1 grid-flow-col md:justify-between md:mx-[5%]">
-        <div class="flex items-center justify-center gap-3 px-2 mx-2">
+        <router-link to="/" class="flex items-center justify-center gap-3 px-2 mx-2">
             <div>
                 <img class="w-[40px]" src="@/assets/images/logo.png" />
             </div>
             <div>
                 <span class="text-lg font-bold text-blue-main">Travel Taiwan</span>
             </div>
-        </div>
+        </router-link>
+
         <div class="flex items-center justify-center px-2 mx-2">
             <router-link to="/" class="btn btn-ghost btn-lg rounded-btn">首頁</router-link>
             <router-link to="Guide" class="btn btn-ghost btn-lg rounded-btn">景點導覽</router-link>
